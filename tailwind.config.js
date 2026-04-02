c219ae79551716f1eedf41eb884b2cf86c2ab81b
Ourajeenn <jenoura17@gmail.com>
2026-04-02 12:34:21 +0200

    Initial commit for Beastborne RPG

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        monda: ['Monda', 'sans-serif'],
+        heading: ['Cinzel', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
